71ceb55a24aea0fc67c0ab092909ab809242c824
Taump <taump@yandex.ru>
2022-05-21 18:51:08 +0300

    change factory

--- conf.js
+++ conf.js
@@ -8,7 +8,7 @@ exports.webPort = null;
 
 exports.webserverPort = 3005;
 exports.testnet = process.env.testnet == "1";
-exports.factory_aa = "DNANYYJQHBRI2TLZHBTBXTM7Z26P7PMO";
+exports.factory_aa = "X57IVRKVCRTBZM6KMYL6RCRG4P4MWJZN";
 exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
 exports.forward_aa = "E4BAASPOCW6WHXSUOY2XEKXKA42RRD5I";
 exports.token_registry_aa_address = process.env.testnet ? "O6H6ZIFI57X3PLTYHOCVYPP5A553CYFQ" : "O6H6ZIFI57X3PLTYHOCVYPP5A553CYFQ";
