779e134d41b823150bc2be14c0c6e17f78a7bbdf
Taump <taump@yandex.ru>
2022-07-07 20:34:51 +0700

    fix

--- webserver/controllers/championshipsController.js
+++ webserver/controllers/championshipsController.js
@@ -3,7 +3,14 @@ const { sportDataService } = require("../../SportData");
 module.exports = async (request, reply) => {
   try {
     const sportInParams = request.params.sport;
-    return reply.send(sportDataService.getChampionships(sportInParams));
+    const championshipInParams = request.params.championship || null;
+
+    if (championship && sportInParams) {
+      return sportDataService.getChampionshipInfo(sportInParams, championship);
+    } else {
+      return reply.send(sportDataService.getChampionships(sportInParams));
+    }
+
   } catch {
     return reply.internalServerError();
   }
