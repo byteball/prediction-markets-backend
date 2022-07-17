0380279ed1d12a9d5ef64a639475d67ec4e550ff
Taump <taump@yandex.ru>
2022-07-17 16:52:33 +0700

    fix aa

--- conf.js
+++ conf.js
@@ -8,7 +8,7 @@ exports.webPort = null;
 
 exports.webserverPort = 3001;
 exports.testnet = process.env.testnet == "1";
-exports.factory_aa = "ZPOO7GQ6X4L7STBRSI4PVLEYEAYQ6CZE";
+exports.factory_aa = "QSARHZ2WGMNRY2YO5QWQHT3RPXHVDTVS";
 exports.hub = process.env.testnet ? 'obyte.org/bb-test' : 'obyte.org/bb';
 exports.forward_aa = "E4BAASPOCW6WHXSUOY2XEKXKA42RRD5I";
 exports.token_registry_aa_address = process.env.testnet ? "O6H6ZIFI57X3PLTYHOCVYPP5A553CYFQ" : "O6H6ZIFI57X3PLTYHOCVYPP5A553CYFQ";
