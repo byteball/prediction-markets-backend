7c08766cd6ec42a1132aabae3f14037ddebc114e
Taump <taump@yandex.ru>
2022-09-04 13:46:53 +0400

    fix

--- db/api/getActualMarketInfo.js
+++ db/api/getActualMarketInfo.js
@@ -2,5 +2,5 @@ const db = require('ocore/db.js');
 
 exports.getActualMarketInfo = async function (aa_address) {
   const data = await db.query('SELECT yes_price, no_price, draw_price, reserve, supply_yes, supply_no, supply_draw, coef FROM trades WHERE aa_address=? ORDER BY timestamp DESC LIMIT 1', [aa_address]);
-  return data[0];
+  return data[0] || [];
 }
